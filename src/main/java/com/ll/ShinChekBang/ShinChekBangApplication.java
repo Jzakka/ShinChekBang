1f126c0d0444c3df5a76f4b9161cc4ea583ef927
Jzakka <mouse4786@gmail.com>
2023-05-20 16:09:32 +0900

    setup: 프로젝트 셋업

---
+++ src/main/java/com/ll/ShinChekBang/ShinChekBangApplication.java
@@ -0,0 +1,13 @@
+package com.ll.ShinChekBang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShinChekBangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShinChekBangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/ShinChekBang/ShinChekBangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.ShinChekBang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShinChekBangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
